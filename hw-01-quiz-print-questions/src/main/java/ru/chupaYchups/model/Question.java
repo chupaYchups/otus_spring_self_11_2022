37d180a60e7567676dbe21d2e2b69250ee7f5a1f
chupaYchups <chupaYchups@gmail.com>
2022-11-20 23:08:01 +0300

    add questions

--- hw-01-quiz-print-questions/src/main/java/ru/chupaYchups/model/Question.java
+++ hw-01-quiz-print-questions/src/main/java/ru/chupaYchups/model/Question.java
@@ -1,8 +1,16 @@
 package ru.chupaYchups.model;
 
+import lombok.Builder;
 import lombok.Data;
+import lombok.Singular;
+
+import java.util.List;
 
 @Data
+@Builder
 public class Question {
-    private final String question;
+    private final String questionText;
+    private final int rightAnswerNum;
+    @Singular("answerVariant")
+    private final List<String> answerVariantList;
 }
