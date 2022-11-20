47cf08df985fc6f13d25a7e1343dbddb1ffd1b1a
chupaYchups <chupaYchups@gmail.com>
2022-11-20 20:08:04 +0300

    add simple hello world

---
+++ hw-01-quiz-print-questions/src/main/java/ru/chupaYchups/service/PersonService.java
@@ -0,0 +1,6 @@
+package ru.chupaYchups.service;
+
+public interface PersonService {
+
+    void hello();
+}
---
+++ hw-01-quiz-print-questions/src/main/java/ru/chupaYchups/service/PersonServiceImpl.java
@@ -0,0 +1,10 @@
+package ru.chupaYchups.service;
+
+public class PersonServiceImpl implements PersonService{
+
+    @Override
+    public void hello() {
+
+        System.out.println("Hello!");
+    }
+}
